b867fa7b742b099c8dcfbe6df5462ab9dc49f17e
TamasMezo <tamas.mezo@codecrew.hu>
2024-11-20 20:01:14 +0100

    feature: add project architecture

---
+++ samwell_ai/apps/img-analyzer-be/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'img-analyzer-be',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/img-analyzer-be',
+};
